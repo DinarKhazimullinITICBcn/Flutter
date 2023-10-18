0270b66f8a870af5329154d97afe369cc1607299
DinarKhazimullinITICBcn <146452935+DinarKhazimullinITICBcn@users.noreply.github.com>
2023-10-18 19:12:39 +0200

    Exercici fet complet

---
+++ Exercici_Llista_V1.3/main.dart
@@ -0,0 +1,151 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter TextField y ListView',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final TextEditingController _textEditingController = TextEditingController();
+  final List<String> _items = [];
+
+  void _addItemToList(String newItem) {
+    setState(() {
+      _items.add(newItem);
+      _textEditingController.clear();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('TextField y ListView'),
+      ),
+      body: ElMeuBody(
+        textEditingController: _textEditingController,
+        addItemToList: _addItemToList,
+        items: _items,
+      ),
+    );
+  }
+}
+
+class ElMeuBody extends StatelessWidget {
+  final TextEditingController textEditingController;
+  final List<String> items;
+  final Function(String) addItemToList;
+
+  ElMeuBody({
+    required this.textEditingController,
+    required this.items,
+    required this.addItemToList,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: TextField(
+            controller: textEditingController,
+            decoration: InputDecoration(
+              labelText: 'Ingresi un article',
+              border: OutlineInputBorder(),
+            ),
+            onSubmitted: (text) {
+              if (text.isNotEmpty) {
+                addItemToList(text);
+              }
+            },
+          ),
+        ),
+        Expanded(
+          child: ListView.builder(
+            itemCount: items.length,
+            itemBuilder: (BuildContext context, int index) {
+              return ListTile(
+                leading: Icon(Icons.shopping_cart),
+                title: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Text(items[index]),
+                    ComptadorEnter()
+                  ],
+                )
+              );  
+            }
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class ComptadorEnter extends StatefulWidget  {
+  const ComptadorEnter({super.key});
+
+  @override
+  _ComptadorEnterState createState() => _ComptadorEnterState();
+}
+
+class _ComptadorEnterState extends State<ComptadorEnter> {
+  int comptador = 1;
+
+  void _incrementa() {
+    setState(() {
+      comptador++;
+    });
+  }
+
+  void _decrementa() {
+    setState(() {
+      if (comptador > 0) {
+        comptador--;
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Text('$comptador'),
+        Column(
+          children: [
+            IconButton(
+              onPressed: _incrementa, 
+              icon: Icon(Icons.add)
+            ),
+            IconButton(
+              onPressed: _decrementa, 
+              icon: Icon(Icons.remove)
+            )
+          ]
+        ),
+        IconButton(
+          onPressed: _decrementa,
+          icon: Icon(Icons.delete)
+        )
+      ],
+    );
+  }
+}
